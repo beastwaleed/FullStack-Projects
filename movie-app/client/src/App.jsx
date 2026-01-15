984454e57c2bf48c5205316122f2108bd9de2d7f
beastwaleed <waleedafzalshaikh@gmail.com>
2026-01-15 21:56:15 +0500

    fixed app.js

--- movie-app/client/src/App.jsx
+++ movie-app/client/src/App.jsx
@@ -4,9 +4,9 @@ import Spinner from './components/spinner'
 import { useDebounce } from 'react-use'
 import { Routes, Route} from 'react-router-dom'
 import Home from "./pages/Home";
-import Favorites from "./src/pages/Favorites";
+import Favorites from "./pages/Favorites.jsx";
 import Navbar from './components/Navbar'
-import Watch from './src/pages/Watch'
+import Watch from './pages/Watch.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
