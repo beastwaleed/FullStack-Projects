2bbd8288bdd4e2e244d09ad130ba43ebbdde5bbc
beastwaleed <waleedafzalshaikh@gmail.com>
2026-02-02 21:35:03 +0500

    Project is live

--- AI-Thumbnail-Generator/server/server.ts
+++ AI-Thumbnail-Generator/server/server.ts
@@ -22,7 +22,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://your-frontend.vercel.app'],
+    origin: ['http://localhost:5173', 'https://thumbify-nu.vercel.app/'],
     credentials: true
 }))
 
